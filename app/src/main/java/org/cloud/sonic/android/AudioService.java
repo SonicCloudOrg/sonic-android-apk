6f5e74832da72f20f248e6bd954876611c749a31
ZhouYixun <291028775@qq.com>
2022-01-26 00:33:28 +0800

    加上头部

--- app/src/main/java/org/cloud/sonic/android/AudioService.java
+++ app/src/main/java/org/cloud/sonic/android/AudioService.java
@@ -30,7 +30,9 @@ import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.Date;
 
 /**
  * @author Eason, sndcpy
@@ -58,6 +60,7 @@ public class AudioService extends Service {
     private MediaProjectionManager mediaProjectionManager;
     private MediaProjection mediaProjection;
     private Thread recorderThread;
+    private long presentationTimeUs;
 
     @SuppressLint("NewApi")
     public static void start(Context context, Intent data) {
@@ -71,7 +74,7 @@ public class AudioService extends Service {
     @SuppressLint("NewApi")
     public void onCreate() {
         super.onCreate();
-
+        initTransferACC();
         Notification notification = createNotification(false);
 
         NotificationChannel channel = new NotificationChannel(CHANNEL_ID, getString(R.string.app_name), NotificationManager.IMPORTANCE_NONE);
@@ -178,11 +181,11 @@ public class AudioService extends Service {
 
     private MediaCodec mediaCodec;
 
-    private void initTransferACC(){
+    private void initTransferACC() {
         MediaFormat encodeFormat = MediaFormat.createAudioFormat(MediaFormat.MIMETYPE_AUDIO_AAC, 16000, 1);//参数对应-> mime type、采样率、声道数
         encodeFormat.setInteger(MediaFormat.KEY_BIT_RATE, 128 * 100);//比特率
         encodeFormat.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectLC);
-        encodeFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 16*1024);
+        encodeFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 16 * 1024);
         try {
             mediaCodec = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC);
         } catch (IOException e) {
@@ -192,43 +195,76 @@ public class AudioService extends Service {
         mediaCodec.start();
     }
 
+    private MediaCodec.BufferInfo encodeBufferInfo;
+    private ByteBuffer[] encodeOutputBuffers ;
+    private ByteBuffer[] encodeInputBuffers  ;
+
     @SuppressLint("NewApi")
-    private void encodePCMToAAC(byte[] bytes) throws IOException {
+    private void encodePCMToAAC(byte[] bytes, OutputStream outputStream) throws IOException {
+        encodeBufferInfo = new MediaCodec.BufferInfo();
+        ByteBuffer inputBuffer;
+        ByteBuffer outputBuffer;
+        byte[] chunkAudio;
+        encodeInputBuffers = mediaCodec.getInputBuffers();
+        encodeOutputBuffers = mediaCodec.getOutputBuffers();
+        int outBitSize;
+        int outputIndex;
+        int outPacketSize;
+
         int inputBufIndex = mediaCodec.dequeueInputBuffer(1000);
         if (inputBufIndex >= 0) {
-            ByteBuffer mInputBuffer = mediaCodec.getInputBuffer(inputBufIndex);
-            mInputBuffer.clear();
-            mInputBuffer.put(bytes, 0, 1024 * 1024);
-            mediaCodec.queueInputBuffer(inputBufIndex, 0, 1024 * 1024, (1000000 * mEncodedSize / AUDIO_BYTE_PER_SAMPLE), 0);
-            mEncodedSize += BUFFER_SIZE_IN_BYTES;
+            inputBuffer = encodeInputBuffers[inputBufIndex];
+            inputBuffer.clear();
+            inputBuffer.put(bytes);
+            mediaCodec.queueInputBuffer(inputBufIndex, 0, bytes.length, 0, 0);
         }
 
-        int outputBufIndex = mAudioEncoder.dequeueOutputBuffer(mOutBufferInfo, 1000);
-        if (outputBufIndex >= 0) {
-            mOutBuffer = mAudioOutputBuffers[outputBufIndex];
-            if ((mOutBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
-                // 这里可以从ByteBuffer中获取csd参数
-                mAudioEncoder.releaseOutputBuffer(outputBufIndex, false);
-                return;
+        //通过dequeueOutputBuffer(BufferInfo info, long timeoutUs)来请求一个输出缓存,传入一个上面的BufferInfo对象
+        outputIndex = mediaCodec.dequeueOutputBuffer(encodeBufferInfo, 10000);
+        //然后通过返回的index得到输出缓存，并通过BufferInfo获取ByteBuffer的信息
+        while (outputIndex >= 0) {
+            outBitSize = encodeBufferInfo.size;
+
+            //添加ADTS头,ADTS头包含了AAC文件的采样率、通道数、帧数据长度等信息。
+            outPacketSize = outBitSize + 7;//7为ADTS头部的大小
+            outputBuffer = encodeOutputBuffers[outputIndex];//拿到输出Buffer
+            outputBuffer.position(encodeBufferInfo.offset);
+            outputBuffer.limit(encodeBufferInfo.offset + outBitSize);
+            chunkAudio = new byte[outPacketSize];
+            addADTStoPacket(chunkAudio, outPacketSize);//添加ADTS 代码后面会贴上
+            outputBuffer.get(chunkAudio, 7, outBitSize);//将编码得到的AAC数据 取出到byte[]中偏移量offset=7
+            outputBuffer.position(encodeBufferInfo.offset);
+            //showLog("outPacketSize:" + outPacketSize + " encodeOutBufferRemain:" + outputBuffer.remaining());
+            try {
+                outputStream.write(chunkAudio, 0, chunkAudio.length);//BufferOutputStream 将文件保存到内存卡中 *.aac
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            if (mOutBufferInfo.size != 0 && mLastAudioPresentationTimeUs < mOutBufferInfo.presentationTimeUs) {
-                mOutBuffer.position(mOutBufferInfo.offset);
-                mOutBuffer.limit(mOutBufferInfo.offset + mOutBufferInfo.size);
+            //releaseOutputBuffer方法必须调用
+            mediaCodec.releaseOutputBuffer(outputIndex, false);
+            outputIndex = mediaCodec.dequeueOutputBuffer(encodeBufferInfo, 10000);
 
-                addADTStoPacket(mADTSHeader, mOutBufferInfo.size + 7);
-                mMixedFileOutputStream.write(mADTSHeader);
-
-                mOutBuffer.get(mEncodedBytes, 0, mOutBufferInfo.size);
-                mOutBuffer.clear();
-                mMixedFileOutputStream.write(mEncodedBytes, 0, mOutBufferInfo.size);
-
-                mLastAudioPresentationTimeUs = mOutBufferInfo.presentationTimeUs;
-            }
-
-            mAudioEncoder.releaseOutputBuffer(outputBufIndex, false);
-        } else if (outputBufIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
-            mAudioOutputBuffers = mAudioEncoder.getOutputBuffers();
         }
+    }
+
+    /**
+     * 添加ADTS头
+     *
+     * @param packet
+     * @param packetLen
+     */
+    private void addADTStoPacket(byte[] packet, int packetLen) {
+        int profile = 2; // AAC LC
+        int freqIdx = 8; // 44.1KHz
+        int chanCfg = 1; // CPE
+        // fill in ADTS data
+        packet[0] = (byte) 0xFF;
+        packet[1] = (byte) 0xF9;
+        packet[2] = (byte) (((profile - 1) << 6) + (freqIdx << 2) + (chanCfg >> 2));
+        packet[3] = (byte) (((chanCfg & 3) << 6) + (packetLen >> 11));
+        packet[4] = (byte) ((packetLen & 0x7FF) >> 3);
+        packet[5] = (byte) (((packetLen & 7) << 5) + 0x1F);
+        packet[6] = (byte) 0xFC;
     }
 
     //record audio
@@ -246,7 +282,8 @@ public class AudioService extends Service {
                     byte[] buf = new byte[SAMPLE_RATE * CHANNELS * BUFFER_MS / 1000];
                     while (true) {
                         int r = recorder.read(buf, 0, buf.length);
-                        socket.getOutputStream().write(buf, 0, r);
+                        encodePCMToAAC(buf,socket.getOutputStream());
+//                        socket.getOutputStream().write(encodePCMToAAC(buf,socket.getOutputStream()), 0, r);
                     }
                 } catch (IOException e) {
                     // ignore
